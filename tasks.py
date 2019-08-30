6d2f844522ce0e0f46ee6d01c46020d052de8378
Takahiro Ueda <tueda@st.seikei.ac.jp>
2019-08-30 19:57:18 +0900

    [ci] Colorise test output

--- tasks.py
+++ tasks.py
@@ -6,23 +6,23 @@ from invoke import task
 @task
 def fmt(c):  # type: ignore
     """Run formatters."""
-    c.run("black .")
-    c.run("isort -y")
+    c.run("black .", pty=True)
+    c.run("isort -y", pty=True)
 
 
 @task
 def lint(c):  # type: ignore
     """Run linters."""
-    c.run("black --check --diff .")
-    c.run("isort --check-only --diff")
-    c.run("flake8 donuts setup.py tasks.py")
-    c.run("mypy donuts setup.py tasks.py")
+    c.run("black --check --diff .", pty=True)
+    c.run("isort --check-only --diff", pty=True)
+    c.run("flake8 donuts setup.py tasks.py", pty=True)
+    c.run("mypy donuts setup.py tasks.py", pty=True)
 
 
 @task
 def test(c):  # type: ignore
     """Run tests."""
-    c.run("pytest --cov=donuts")
+    c.run("pytest --cov=donuts", pty=True)
 
 
 @task
