d9e38ce1f3ae6e89509cbb9e8cf90358beb3d0ed
Takahiro Ueda <tueda@st.seikei.ac.jp>
2019-10-06 15:26:47 +0900

    chore: upgrade mypy to 0.730

--- donuts/__init__.py
+++ donuts/__init__.py
@@ -1,9 +1,8 @@
 """Python binding to the Donuts wrapper for the Rings library."""
 
-# TODO: this is a workaround for python/mypy#7042. Remove it in the next mypy release.
-from .poly import Polynomial as Polynomial
-from .rat import RationalFunction as RationalFunction
-from .var import Variable as Variable
-from .varset import VariableSet as VariableSet
+from .poly import Polynomial
+from .rat import RationalFunction
+from .var import Variable
+from .varset import VariableSet
 
 __all__ = ("Polynomial", "RationalFunction", "Variable", "VariableSet")
