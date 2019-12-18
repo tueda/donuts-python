66f9b0736e4d2517f3d60f225cc3aa6858c46b7e
Takahiro Ueda <tueda@st.seikei.ac.jp>
2019-12-19 00:28:02 +0900

    build: improve how to create the far JAR file
    By setting the project version to an empty value, we can directly
    generate donuts-all.jar, without an additional file-copy operation.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 import os
 import os.path
-import shutil
 import subprocess
 from distutils.command.build import build as build_  # type: ignore
 from typing import List, Tuple
@@ -10,7 +9,6 @@ from typing import List, Tuple
 from setuptools import Command, find_packages, setup
 
 VERSION = "0.0.1"
-JAR_VERSION = "0.0.1-SNAPSHOT"
 
 
 def readme() -> str:
@@ -26,7 +24,7 @@ class build(build_):  # type: ignore # noqa: N801
 
 
 class build_jar(Command):  # type: ignore # noqa: N801
-    """Command to build JAR files."""
+    """Command to build the JAR file."""
 
     description = "build JAR archives"
     user_options: List[Tuple[str, str, str]] = []
@@ -45,23 +43,22 @@ class build_jar(Command):  # type: ignore # noqa: N801
             setup_dir = os.path.dirname(os.path.abspath(__file__))
             java_dir = os.path.join(setup_dir, "donuts", "java")
             lib_dir = os.path.join(java_dir, "build", "libs")
-            src_jar_file = os.path.join(
-                lib_dir, "donuts-{}-all.jar".format(JAR_VERSION)
-            )
-            dest_jar_file = os.path.join(lib_dir, "donuts-all.jar")
+            jar_file = os.path.join(lib_dir, "donuts-all.jar")
 
             if os.path.isfile(os.path.join(java_dir, "build.gradle")):
-                # Build the fat jar.
+                # Build the fat JAR file by Gradle.
                 if os.name == "posix":
                     gradlew_cmd = "./gradlew"
                 elif os.name == "nt":
                     gradlew_cmd = "gradlew.bat"
-                subprocess.run([gradlew_cmd, "shadowJar"], cwd=java_dir, check=True)
-                # And then copy it.
-                shutil.copyfile(src_jar_file, dest_jar_file)
+                subprocess.run(
+                    [gradlew_cmd, "-Dorg.gradle.project.version=", "shadowJar"],
+                    cwd=java_dir,
+                    check=True,
+                )
 
-            if not os.path.isfile(dest_jar_file):
-                raise OSError("Failed to generate JAR files")
+            if not os.path.isfile(jar_file):
+                raise OSError("Failed to generate the JAR file")
 
 
 setup(
