92ed105b061e4d7671aa55c3fb9c694e82278143
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-01-06 20:39:49 +0900

    refactor: more restricted typing for raw array functions

--- donuts/array.py
+++ donuts/array.py
@@ -1,8 +1,15 @@
 """Raw Java arrays."""
 
-from typing import Any, Iterable, Sequence
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Iterable, Sequence, Union
 
 from .jvm import jvm
+
+if TYPE_CHECKING:
+    from .poly import PolynomialLike
+    from .var import VariableLike
+
 
 _RawVariable = jvm.find_class("com.github.tueda.donuts.Variable")
 _RawPolynomial = jvm.find_class("com.github.tueda.donuts.Polynomial")
@@ -21,16 +28,19 @@ def _create_raw_int_array(values: Sequence[int]) -> Any:
     return array
 
 
-def _create_raw_var_array(variables: Sequence[Any]) -> Any:
+def _create_raw_var_array(
+    variables: Sequence[Union[VariableLike, Iterable[VariableLike]]]
+) -> Any:
     """Create a Java array of variables."""
     from .var import Variable
 
     if len(variables) == 1:
         x = variables[0]
-        if isinstance(x, Sequence) and not isinstance(x, str):
-            return _create_raw_var_array(x)
-        if isinstance(x, Iterable) and not isinstance(x, str):
-            return _create_raw_var_array(tuple(x))
+        if not isinstance(x, str):
+            if isinstance(x, Sequence):
+                return _create_raw_var_array(x)
+            if isinstance(x, Iterable):
+                return _create_raw_var_array(tuple(x))
 
     array = _new_array(_RawVariable, len(variables))
     for i in range(len(variables)):
@@ -44,17 +54,20 @@ def _create_raw_var_array(variables: Sequence[Any]) -> Any:
     return array
 
 
-def _create_raw_poly_array(polynomials: Sequence[Any]) -> Any:
+def _create_raw_poly_array(
+    polynomials: Sequence[Union[PolynomialLike, Iterable[PolynomialLike]]]
+) -> Any:
     """Create a Java array of polynomials."""
     from .poly import Polynomial
     from .var import Variable
 
     if len(polynomials) == 1:
         x = polynomials[0]
-        if isinstance(x, Sequence) and not isinstance(x, (Polynomial, str)):
-            return _create_raw_poly_array(x)
-        if isinstance(x, Iterable) and not isinstance(x, (Polynomial, str)):
-            return _create_raw_poly_array(tuple(x))
+        if not isinstance(x, (Polynomial, str)):
+            if isinstance(x, Sequence):
+                return _create_raw_poly_array(x)
+            if isinstance(x, Iterable):
+                return _create_raw_poly_array(tuple(x))
 
     array = _new_array(_RawPolynomial, len(polynomials))
     for i in range(len(polynomials)):
