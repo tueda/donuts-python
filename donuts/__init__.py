4848179ea76cb4a0cd602103161dbd2791317fab
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-01-07 14:06:35 +0900

    feat: re-export attributes from the module

--- donuts/__init__.py
+++ donuts/__init__.py
@@ -1,6 +1,6 @@
 """Python binding to the Donuts wrapper for the Rings library."""
 
-from .poly import Polynomial, gcd, lcm, product, sum  # noqa: F401
+from .poly import Polynomial, gcd, lcm, product
 from .rat import RationalFunction
 from .var import Variable
 
@@ -14,3 +14,15 @@ __all__ = (
     "lcm",
     "product",
 )
+
+# The following attributes are explicitly re-exported for mypy with
+# `--no-implicit-reexport`.
+
+# isort: off
+
+from .poly import PolynomialLike as PolynomialLike  # noqa: F401
+from .poly import sum as sum  # noqa: A001,F401
+from .rat import RationalFunctionLike as RationalFunctionLike  # noqa: F401
+from .var import VariableLike as VariableLike  # noqa: F401
+from .varset import VariableSet as VariableSet  # noqa: F401
+from .varset import VariableSetLike as VariableSetLike  # noqa: F401
