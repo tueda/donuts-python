5be23547a617fb5ffb69d47da843e4e35cb3712b
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-12-29 14:30:18 +0900

    chore: bump version to 0.0.1-alpha.1

--- donuts/__init__.py
+++ donuts/__init__.py
@@ -1,6 +1,6 @@
 """Python binding to the Donuts wrapper for the Rings library."""
 
-__version__ = "0.0.1rc1"
+__version__ = "0.0.1-alpha.1"
 
 from .poly import Polynomial, gcd, lcm, product
 from .rat import RationalFunction
