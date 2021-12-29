c87ee8f4f6706d4dfd6b8a2eaa8ceaf3c7c82115
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-12-29 16:57:48 +0900

    docs(readthedocs): changes for Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,15 +11,28 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+import subprocess
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+
+# Check if we are running on Read the Docs' servers.
+read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
+
+# Check if sphinx_rtd_theme is available
+try:
+    import sphinx_rtd_theme  # noqa: F401
+
+    has_sphinx_rtd_theme = True
+except ImportError:
+    has_sphinx_rtd_theme = False
+    pass
 
 
 # -- Project information -----------------------------------------------------
 
 project = "donuts-python"
-copyright = "2019, Takahiro Ueda"  # noqa: A001
+copyright = "2021, Takahiro Ueda"  # noqa: A001
 author = "Takahiro Ueda"
 
 
@@ -35,6 +48,9 @@ extensions = [
     "sphinx_autodoc_typehints",
 ]
 
+if has_sphinx_rtd_theme:
+    extensions.append("sphinx_rtd_theme")
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
@@ -49,13 +65,17 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme" if has_sphinx_rtd_theme else "default"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
+# ----------------------------------------------------------------------------
+
+if read_the_docs_build:
+    subprocess.run(["invoke", "build"])  # noqa: S603, S607
 
 # This hack is needed to suppress "WARNING: autodoc: failed to determine
 # JavaObject id=oxx to be documented.the following exception was raised:
