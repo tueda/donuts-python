f1002f3982f27ce6ec32a47a664d43cd5532f87a
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2024-02-10 17:38:32 +0900

    chore: bump version to 0.0.6a0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class BuildJarCommand(Command):
 
 setup(
     name="donuts-python",
-    version="0.0.5",
+    version="0.0.6a0",
     description="Python binding to Donuts",
     long_description=readme(),
     author="Takahiro Ueda",
